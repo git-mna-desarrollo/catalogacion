714b90c36e2e88567384ad726ab5d356775a7359
Cristiam Herrera Daza <47664718+cristiamherreradaza@users.noreply.github.com>
2021-10-07 11:19:52 -0400

    Se adicionaron los modelos de las tablas base

--- app/Http/Controllers/PatrimonioController.php
+++ app/Http/Controllers/PatrimonioController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Localidad;
+use App\Provincia;
+use App\Departamento;
+use App\Tecnicamaterial;
 use Illuminate\Http\Request;
 
 class PatrimonioController extends Controller
 {
     public function formulario()
     {
+        $departamentos = Departamento::all();
+        $localidades = Localidad::all();
+        $provincias = Provincia::all();
+        $tecnicas = Tecnicamaterial::all();
+        dd($tecnicas);
         return view('patrimonio.formulario');
     }
 }
