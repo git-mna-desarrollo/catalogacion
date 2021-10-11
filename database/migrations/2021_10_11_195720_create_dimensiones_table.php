3c5a5212cb74d75d82ba4f3b0295467be2ab54c1
Cristiam Herrera Daza <47664718+cristiamherreradaza@users.noreply.github.com>
2021-10-11 16:06:49 -0400

    Se crearon las migraciones para ubiaciones y dimensiones

---
+++ database/migrations/2021_10_11_195720_create_dimensiones_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDimensionesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dimensiones', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('creador_id')->nullable();
+            $table->foreign('creador_id')->references('id')->on('users');
+            $table->unsignedBigInteger('modificador_id')->nullable();
+            $table->foreign('modificador_id')->references('id')->on('users');
+            $table->unsignedBigInteger('eliminador_id')->nullable();
+            $table->foreign('eliminador_id')->references('id')->on('users');
+            $table->unsignedBigInteger('patrimonio_id')->nullable();
+            $table->foreign('patrimonio_id')->references('id')->on('patrimonios');
+            $table->decimal('alto', 15, 2)->nullable();
+            $table->decimal('ancho', 15, 2)->nullable();
+            $table->decimal('largo', 15, 2)->nullable();
+            $table->decimal('profundidad', 15, 2)->nullable();
+            $table->decimal('diametro', 15, 2)->nullable();
+            $table->decimal('peso', 15, 2)->nullable();
+            $table->decimal('circunferencia', 15, 2)->nullable();
+            $table->string('estado')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dimensiones');
+    }
+}
